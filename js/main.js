60f2348615357148f7e0cb1d7ffe3c4f44cdc094
roger-rocha <rogerochabnusc@gmail.com>
2021-01-07 22:11:50 -0300

    consulta cep

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/"
+    
+    console.log(url)
+    
+    $.ajax({
+       url: url,
+       type: "GET",
+       success: function(response){
+           console.log(response);
+           $("#logradouro").html(response.logradouro);
+           $("#bairro").html(response.bairro);
+           $("#localidade").html(response.localidade);
+           $("#uf").html(response.uf);
+           $("#titulo_cep").html( "CEP " + response.cep);
+           $(".cep").show();       
+        }
+
+   })
+}
+
+$(function(){
+
+$(".cep").hide();
+});
